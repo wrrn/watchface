38e686e947fae7560a0a74f280e08a4f42121d68
Warren Harper <warrenjharper@gmail.com>
2016-07-28 21:44:20 -0400

    Update the time every minute

--- src/watchface.c
+++ src/watchface.c
@@ -3,6 +3,28 @@
 static  Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time(struct tm *tick_time) {
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer),
+           clock_is_24h_style() ? "%H:%M" :
+                                  "%I:%M",
+           tick_time
+           );
+  text_layer_set_text(s_time_layer, s_buffer);
+
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time(tick_time);
+}
+
+static void display_current_time() {
+  time_t temp = time(NULL);
+  update_time(localtime(&temp));
+}
+
+
+
 
 
 static void main_window_load(Window *window) {
@@ -42,8 +64,15 @@ static Window* init() {
       .load = main_window_load,
         .unload = main_window_unload
         });
+
+  // Register the TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
   // Show the window on the watch, with animated=true
   window_stack_push(w, true);
+
+  //Make sure the time is displayed from the start
+  display_current_time();
   return w;
 }
 
